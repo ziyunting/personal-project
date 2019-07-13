52cfe3911f4f4b7495eba96fbb04f128bd08330b
zhengfukang <852059020@qq.com>
2019-07-13 22:57:52 +0800

    laptop version

--- WX/myFirstDemo/pages/list/list.js
+++ WX/myFirstDemo/pages/list/list.js
@@ -7,23 +7,39 @@ Page({
     data: {
         date: "十月七日",
         title: "点评人荐文：居安思危的前瞻性",
-        imgSrc: "/image/list/recommend-image.png"
+        imgSrc: "/image/list/recommend-image.png",
+        aaa: true,
+        listLike: {
+            0: true,
+            1: false,
+            2: false,
+            3: true
+        }
     },
     test() {
         wx.showActionSheet({
-            itemList: ["内容过期了",'内容和深度阅读不相关','不再显示来自深度阅读的内容'],
-            success:res => {
-
+            itemList: ["内容过期了", '内容和深度阅读不相关', '不再显示来自深度阅读的内容'],
+            success: res => {
 
 
             }
         })
     },
+    onLikeTap: function () {
+        var temp = this.data.aaa;
+        temp = !temp;
+        this.setData({aaa: temp});
+    },
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
+wx.request({
+  url: 'https://easy-mock.com/mock/5bd149fab36f2c5eac3a9274/QM_magazine/getRecommendInfo',
+  success:function(res){
+    console.log(res.data);
+  }
+})
     },
 
     /**
