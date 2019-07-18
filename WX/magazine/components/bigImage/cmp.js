a2e8e0672c885ef7cd8fffef34fd9c3fae6697bb
zhengfukang <852059020@qq.com>
2019-07-18 20:45:41 +0800

    miniprogram refactor by commponent

---
+++ WX/magazine/components/bigImage/cmp.js
@@ -0,0 +1,35 @@
+// components/bigImage/cmp.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    imageSrc:String,
+    mainTitle:String,
+    // imageSrc: {
+    //   type: String,
+    //   value: "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1718395925,3485808025&fm=27&gp=0.jpg",
+    //   observer: function(newValue, oldValue, changePath) {}
+    // },
+    // mainTitle: {
+    //   type: String,
+    //   value: "愿你遍历山河，觉得人间值得",
+    //   observer: function (newValue, oldValue, changePath) { }
+    // }
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
