0e2044b780639d4c18edfd4fa0262351acebc776
zhengfukang <852059020@qq.com>
2019-07-21 23:45:54 +0800

    miniprogram components

---
+++ WX/magazine/components/tag/cmp.js
@@ -0,0 +1,38 @@
+// components/tag/cmp.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    tag: String,
+    tagId: Number
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    onTap() {
+      this._showError();
+      // const tagId = this.properties.tagId
+      // wx.navigateTo({
+      //   url: `/pages/type/type?tageId=${tagId}`,
+      // })
+    },
+    _showError() {
+      wx.showToast({
+        title: "当前小程序为测试版本，不能点击跳转",
+        icon: "none",
+        duration: 1000,
+        mask: true
+      })
+    }
+  }
+})
--- WX/magazine/components/video/cmp.js
+++ WX/magazine/components/video/cmp.js
@@ -4,20 +4,49 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    src: String,
+    poster: String,
+    duration: String,
+    mainTitle: String,
+    videoId: String
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    showPoster: true,
+  },
+  lifetimes: {
+    attached() {
+      this._getVedioInfo();
+    },
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    onPlay() {
+      this._toggleVideoPoster();
+      this.video.play();
+    },
+    onMaskTap() {
+      this._toggleVideoPoster();
+      this.video.seek(0);
+      this.video.stop();
+    },
+    onVedioEnd() {
+      this._toggleVideoPoster();
+    },
+    _getVedioInfo() {
+      const id = this.properties.videoId;
+      this.video = wx.createVideoContext(id, this)
+    },
+    _toggleVideoPoster() {
+      this.setData({
+        showPoster: !this.data.showPoster
+      })
+    },
   }
 })
